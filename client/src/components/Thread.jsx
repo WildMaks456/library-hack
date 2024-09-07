26bc428c455d57aa523689cec4fab53f583dadc5
tredf31 <tredf1275@gmail.com>
2024-09-07 16:17:43 +0500

    styles

--- client/src/components/Thread.jsx
+++ client/src/components/Thread.jsx
@@ -1,5 +1,17 @@
 export const Thread = () => {
     return (
-        <div></div>
+        <div className="thread">
+            <div className="thread-info">
+                <div className="avatar"></div>
+                <div className="thread-author"></div>
+                <div className="thread-date"></div>
+            </div>
+            <div className="thread-header"></div>
+            <div className="thread-body"></div>
+
+            <div className="thread-footer">
+
+            </div>
+        </div>
     );
 };
