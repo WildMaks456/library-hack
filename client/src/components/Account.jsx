12893f3bfd2de4028fbd4db2496fb304c6a2c027
SergeiSmetanin1 <sergejsmetanin11@gmail.com>
2024-09-07 13:45:31 +0600

    add qwe

--- client/src/components/Account.jsx
+++ client/src/components/Account.jsx
@@ -4,7 +4,8 @@ import { NavLink } from 'react-router-dom';
 export default function Account() {
 	return (
     <div>
-      <h2>Личный кабинет....</h2>
+      <h2>Личный кабинет...</h2>
+      <p>qwe</p>
       <p>У вас уже есть аккаунт? <NavLink to="/login">Аккаунт</NavLink> или хотите <NavLink to="/register">зарегаться</NavLink>?</p>
     </div>
   );
